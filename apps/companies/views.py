0dd3e67e1f6a8fe65e9a1f2b47c4ec77c1438530
Pablo1653 <fsanguin@agro.uba.ar>
2026-07-04 14:47:55 -0300

    Create companies  app and home page

--- apps/companies/views.py
+++ apps/companies/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Company
 
-# Create your views here.
+
+def company_list(request):
+    """
+    Display all registered companies.
+    """
+    companies = Company.objects.all()
+
+    context = {
+        "companies": companies
+    }
+
+    return render(
+        request,
+        "companies/company_list.html",
+        context,
+    )
